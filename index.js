1cfa1d76a4c5b797774c4d1d9f515aa911aad5fd
Justin Dennison <justin1dennison@gmail.com>
2017-01-23 20:53:14 -0500

    wired up base routes for login, signup and home html files

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var server = express();
+var port = 8080;
+
+server.get('/', function(request, response){
+  response.sendFile('index.html', {root: __dirname + '/public/html/'});
+});
+
+server.get('/login', function(request, response){
+  response.sendFile('login.html', { root: __dirname + '/public/html/'});
+});
+
+server.get('/signup', function(request, response){
+  response.sendFile('signup.html', { root: __dirname + '/public/html/'});
+});
+
+server.listen(port, function(){
+  console.log("Now listening on port", port);
+});
