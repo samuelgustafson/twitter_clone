21c17d7b41f7534918057b5db98da1f10788b993
samuelgustafson <samelgw@gmail.com>
2017-01-27 17:53:41 -0500

    different things and different stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var server = express();
 var port = 8080;
 
+server.use(express.static(__dirname + '/public'));
+
 server.get('/', function(request, response){
   response.sendFile('index.html', {root: __dirname + '/public/html/'});
 });
